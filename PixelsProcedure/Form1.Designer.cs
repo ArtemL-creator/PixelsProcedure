18e1c6f39dcaa30b750e274de221c3e8454ba35e
User <User@HOME-PC>
2023-09-15 19:37:07 +0300

    Добавьте файлы проекта.

---
+++ PixelsProcedure/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace PixelsProcedure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(735, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(880, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Картинка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(880, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Грейскал";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(880, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Контраст+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(880, 95);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Контраст-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(764, 69);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(52, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(822, 69);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(52, 20);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            205,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(764, 98);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(52, 20);
+            this.numericUpDown3.TabIndex = 7;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(822, 98);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(52, 20);
+            this.numericUpDown4.TabIndex = 8;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            205,
+            0,
+            0,
+            0});
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(764, 124);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(110, 45);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.Value = 1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(880, 124);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Гамма";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(880, 153);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Яркость";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(880, 182);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Негатив";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(880, 211);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Бинор-я";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(822, 156);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(52, 20);
+            this.numericUpDown5.TabIndex = 14;
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(822, 185);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(52, 20);
+            this.numericUpDown6.TabIndex = 15;
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(822, 211);
+            this.numericUpDown7.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(52, 20);
+            this.numericUpDown7.TabIndex = 16;
+            this.numericUpDown7.Value = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 541);
+            this.Controls.Add(this.numericUpDown7);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+    }
+}
+
---
+++ PixelsProcedure/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PixelsProcedure
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Bitmap gray;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(@"D:\Images\4f6ad752090bf5ae323bab7bc37e25e9(2).bmp");
+            pictureBox1.Image = bmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gray = new Bitmap(bmp.Width, bmp.Height);
+            for (int x = 0; x < gray.Width; x++)
+            {
+                for (int y = 0; y < gray.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    byte g = (byte)(0.3f * c.R + 0.59f * c.G + 0.11f * c.B);
+                    gray.SetPixel(x, y, Color.FromArgb(g, g, g));
+                }
+            }
+            pictureBox1.Image = gray;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int Q1 = (int)numericUpDown1.Value;
+            int Q2 = (int)numericUpDown2.Value;
+
+            gray = new Bitmap(bmp.Width, bmp.Height);
+            for (int x = 0; x < gray.Width; x++)
+            {
+                for (int y = 0; y < gray.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    byte g = (byte)(0.3f * c.R + 0.59f * c.G + 0.11f * c.B);
+
+                    if (g < Q1) g = 0;
+                    else if (g > Q2) g = 255;
+                    else g = (byte)((float)(g - Q1) / (Q2 - Q1) * 255);
+
+                    gray.SetPixel(x, y, Color.FromArgb(g, g, g));
+                }
+            }
+            pictureBox1.Image = gray;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int Q1 = (int)numericUpDown3.Value;
+            int Q2 = (int)numericUpDown4.Value;
+
+            gray = new Bitmap(bmp.Width, bmp.Height);
+            for (int x = 0; x < gray.Width; x++)
+            {
+                for (int y = 0; y < gray.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    byte g = (byte)(0.3f * c.R + 0.59f * c.G + 0.11f * c.B);
+                    g = (byte)((float)g / 255 * (Q2 - Q1) + Q1);
+
+                    gray.SetPixel(x, y, Color.FromArgb(g, g, g));
+                }
+            }
+            pictureBox1.Image = gray;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int pow = (int)trackBar1.Value;
+            gray = new Bitmap(bmp.Width, bmp.Height);
+            for (int x = 0; x < gray.Width; x++)
+            {
+                for (int y = 0; y < gray.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    byte g = (byte)(0.3f * c.R + 0.59f * c.G + 0.11f * c.B);
+                    g = (byte)(255 * Math.Pow((float)g / 255, pow));
+                    gray.SetPixel(x, y, Color.FromArgb(g, g, g));
+                }
+            }
+            pictureBox1.Image = gray;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int b = (int)numericUpDown5.Value;
+            gray = new Bitmap(bmp.Width, bmp.Height);
+
+            for (int x = 0; x < gray.Width; x++)
+            {
+                for (int y = 0; y < gray.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    byte g = (byte)(0.3f * c.R + 0.59f * c.G + 0.11f * c.B);
+                    if (b < 0 && g + b < 0) g = 0;
+                    else if (b > 0 && g + b > 255) g = 255;
+                    else g = (byte)((int)g + b);
+
+                    gray.SetPixel(x, y, Color.FromArgb(g, g, g));
+                }
+            }
+            pictureBox1.Image = gray;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int p = (int)numericUpDown6.Value;
+            gray = new Bitmap(bmp.Width, bmp.Height);
+
+            for (int x = 0; x < gray.Width; x++)
+            {
+                for (int y = 0; y < gray.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    byte g = (byte)(0.3f * c.R + 0.59f * c.G + 0.11f * c.B);
+                    if (g >= p) g = (byte)(255 - (int)g);
+
+                    gray.SetPixel(x, y, Color.FromArgb(g, g, g));
+                }
+            }
+            pictureBox1.Image = gray;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int p = (int)numericUpDown7.Value;
+            gray = new Bitmap(bmp.Width, bmp.Height);
+
+            for (int x = 0; x < gray.Width; x++)
+            {
+                for (int y = 0; y < gray.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    byte g = (byte)(0.3f * c.R + 0.59f * c.G + 0.11f * c.B);
+                    if (g <= p) g = 0;
+                    else g = 255;
+
+                    gray.SetPixel(x, y, Color.FromArgb(g, g, g));
+                }
+            }
+            pictureBox1.Image = gray;
+        }
+    }
+}
