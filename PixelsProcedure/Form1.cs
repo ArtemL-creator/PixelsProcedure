8fed4ed9cbc83d4e71e768970c0703665d0cd44e
ArtemL-creator <aalachkov@edu.tversu.ru>
2023-11-09 10:05:18 +0300

    функция "Graph"

--- PixelsProcedure/Form1.cs
+++ PixelsProcedure/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace PixelsProcedure
 {
@@ -30,6 +24,15 @@ namespace PixelsProcedure
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Graph(int[] pixels)
+        {
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                Console.WriteLine(i + ": " + pixels[i]);
+                chart1.Series[0].Points.AddXY(i, pixels[i]);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -57,11 +60,7 @@ namespace PixelsProcedure
                 }
             }
             pictureBox1.Image = gray;
-            for (int i = 0; i < pixels.Length; i++)
-            {
-                Console.WriteLine(i + ": " + pixels[i]);
-                chart1.Series[0].Points.AddXY(i, pixels[i]);
-            }
+            Graph(pixels);
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -89,11 +88,7 @@ namespace PixelsProcedure
                 }
             }
             pictureBox1.Image = gray;
-            for (int i = 0; i < pixels.Length; i++)
-            {
-                Console.WriteLine(i + ": " + pixels[i]);
-                chart1.Series[0].Points.AddXY(i, pixels[i]);
-            }
+            Graph(pixels);
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -118,11 +113,7 @@ namespace PixelsProcedure
                 }
             }
             pictureBox1.Image = gray;
-            for (int i = 0; i < pixels.Length; i++)
-            {
-                Console.WriteLine(i + ": " + pixels[i]);
-                chart1.Series[0].Points.AddXY(i, pixels[i]);
-            }
+            Graph(pixels);
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -145,11 +136,7 @@ namespace PixelsProcedure
                 }
             }
             pictureBox1.Image = gray;
-            for (int i = 0; i < pixels.Length; i++)
-            {
-                Console.WriteLine(i + ": " + pixels[i]);
-                chart1.Series[0].Points.AddXY(i, pixels[i]);
-            }
+            Graph(pixels);
         }
 
         private void button6_Click(object sender, EventArgs e)
@@ -174,11 +161,7 @@ namespace PixelsProcedure
                 }
             }
             pictureBox1.Image = gray;
-            for (int i = 0; i < pixels.Length; i++)
-            {
-                Console.WriteLine(i + ": " + pixels[i]);
-                chart1.Series[0].Points.AddXY(i, pixels[i]);
-            }
+            Graph(pixels);
         }
 
         private void button7_Click(object sender, EventArgs e)
@@ -201,11 +184,7 @@ namespace PixelsProcedure
                 }
             }
             pictureBox1.Image = gray;
-            for (int i = 0; i < pixels.Length; i++)
-            {
-                Console.WriteLine(i + ": " + pixels[i]);
-                chart1.Series[0].Points.AddXY(i, pixels[i]);
-            }
+            Graph(pixels);
         }
 
         private void button8_Click(object sender, EventArgs e)
@@ -229,11 +208,7 @@ namespace PixelsProcedure
                 }
             }
             pictureBox1.Image = gray;
-            for (int i = 0; i < pixels.Length; i++)
-            {
-                Console.WriteLine(i + ": " + pixels[i]);
-                chart1.Series[0].Points.AddXY(i, pixels[i]);
-            }
+            Graph(pixels);
         }
     }
 }
